9f7b22c88d2f8b77a26ec0634815e0761772bd6b
Allexia <alexialcantara@outlook.com>
2020-08-09 23:30:41 -0300

    configured database, finished application

---
+++ src/database/fake_data.js
@@ -0,0 +1,37 @@
+const proffys = [
+    { 
+     name: "Allexia Alcantara",
+     avatar: "https://avatars0.githubusercontent.com/u/48778668?s=460&u=0a692bb8ea3cc8284eaf7a8d99e72d9ae1d18de8&v=4", 
+     whatsapp: "90938407", 
+     bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque corporis quisquam, quo facilis earum ullam quibusdam optio. Possimus voluptates est ullam, itaque dolore tempore inventore, fuga quasi voluptatibus labore a.", 
+     subject: "Developer", 
+     cost: "20", 
+     weekday: [0], 
+     time_from: [720], 
+     time_to: [1220]
+    },
+
+    { 
+        name: "Danny",
+        avatar: "https://avatars0.githubusercontent.com/u/48778668?s=460&u=0a692bb8ea3cc8284eaf7a8d99e72d9ae1d18de8&v=4", 
+        whatsapp: "90938407", 
+        bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque corporis quisquam, quo facilis earum ullam quibusdam optio. Possimus voluptates est ullam, itaque dolore tempore inventore, fuga quasi voluptatibus labore a.", 
+        subject: "Developer", 
+        cost: "1", 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+
+    { 
+        name: "Filipe Magalhães",
+        avatar: "https://avatars2.githubusercontent.com/u/56122079?s=460&u=75db9013afc37d4071960628532d827e54e64339&v=4", 
+        whatsapp: "90938407", 
+        bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque corporis quisquam, quo facilis earum ullam quibusdam optio. Possimus voluptates est ullam, itaque dolore tempore inventore, fuga quasi voluptatibus labore a.", 
+        subject: "Analista de suporte", 
+        cost: "1", 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,96 +1,12 @@
-const proffys = [
-    { 
-     name: "Allexia Alcantara",
-     avatar: "https://avatars0.githubusercontent.com/u/48778668?s=460&u=0a692bb8ea3cc8284eaf7a8d99e72d9ae1d18de8&v=4", 
-     whatsapp: "90938407", 
-     bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque corporis quisquam, quo facilis earum ullam quibusdam optio. Possimus voluptates est ullam, itaque dolore tempore inventore, fuga quasi voluptatibus labore a.", 
-     subject: "Developer", 
-     cost: "20", 
-     weekday: [0], 
-     time_from: [720], 
-     time_to: [1220]
-    },
-
-    { 
-        name: "Danny",
-        avatar: "https://avatars0.githubusercontent.com/u/48778668?s=460&u=0a692bb8ea3cc8284eaf7a8d99e72d9ae1d18de8&v=4", 
-        whatsapp: "90938407", 
-        bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque corporis quisquam, quo facilis earum ullam quibusdam optio. Possimus voluptates est ullam, itaque dolore tempore inventore, fuga quasi voluptatibus labore a.", 
-        subject: "Developer", 
-        cost: "1", 
-        weekday: [0], 
-        time_from: [720], 
-        time_to: [1220]
-    },
-
-    { 
-        name: "Filipe Magalhães",
-        avatar: "https://avatars2.githubusercontent.com/u/56122079?s=460&u=75db9013afc37d4071960628532d827e54e64339&v=4", 
-        whatsapp: "90938407", 
-        bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque corporis quisquam, quo facilis earum ullam quibusdam optio. Possimus voluptates est ullam, itaque dolore tempore inventore, fuga quasi voluptatibus labore a.", 
-        subject: "Analista de suporte", 
-        cost: "1", 
-        weekday: [0], 
-        time_from: [720], 
-        time_to: [1220]
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação Física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-function getSubject(subjectNumber) {
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy (req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses (req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
 const express = require('express')
 const server = express ()
+
+const{
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages') 
 
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
@@ -100,9 +16,10 @@ nunjucks.configure('src/views', {
 
 
 server
+.use(express.urlencoded({ extended: true }))
 .use(express.static("public"))
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
-
+.post("/save-classes", saveClasses)
 .listen(5000)
